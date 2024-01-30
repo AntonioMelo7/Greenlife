d4ae768c3984c364a4762696bc3402198cc6d4ab
Laura Del valle Bermejo <L.delvallebermejo@gmail.com>
2024-01-30 18:03:35 +0100

    Rutas y modelos

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ['USER', 'ADMIN'],
+      default: 'USER',
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'por favor, introduce una dirección de email válida'],
+
+    },
+
+    password: {
+      type: String,
+      required: true,
+      message: "por favor, introduce una contraseña"
+    },
+
+    avatar: {
+      type: String,
+    },
+
+    favorites: {
+      type: [String],
+      default: [],
+      ref: "Esquejes"
+    }
+
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
