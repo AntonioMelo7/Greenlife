38e7dd1f5b19e3e558de22628a9bf7b10f0a13d2
Laura Del valle Bermejo <L.delvallebermejo@gmail.com>
2024-04-20 19:01:28 +0200

    Modelo y rutas Formulario de Huella

--- models/Huella.models.js
+++ models/Huella.models.js
@@ -4,43 +4,43 @@ const huellaSchema = new Schema (
     {
         question1:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 1'],
         },
         question2:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 2'],
         },
         question3:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 3'],
         },
         question4:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 4'],
         },
         question5:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 5'],
         },
         question6:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 6'],
         },
         question7:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 7'],
         },
         question8:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 8'],
         },
         question9:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 9'],
         },
         question10:{
             type: String,
-            required: true,
+            required: [true, 'Es obligatorio responder a la pregunta 10'],
         },
     },
     {timestamps: true,}
--- routes/huella.routes.js
+++ routes/huella.routes.js
@@ -9,4 +9,17 @@ router.get("/getAllHuella", (req, res) =>{
     .catch((err)=> res.status(500).json(err))
 })
 
+router.post("/saveHuella", (req, res) =>{
+    console.log("EntrandoLLamada")
+    console.log(req.body);
+
+    const {  question1, question2, question3, question4, question5,
+        question6, question7, question8, question9, question10 } = req.body;
+
+    Huella.create({question1, question2,question3,question4,question5,
+        question6, question7, question8, question9, question10})
+    .then ((response) => res.json(response))
+    .catch((err)=> res.status(500).json(err))
+})
+
 module.exports = router;
